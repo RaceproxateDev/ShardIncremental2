4d8c0d3de40d68f325c52f51e818b08eea084fe9
RaceproxateDev <coxemeftigtipout@gmail.com>
2026-05-16 10:50:38 +0200

    a

--- utils/UnlockContent.js
+++ utils/UnlockContent.js
@@ -1,7 +1,7 @@
 let constructionContent = document.getElementById("ConstructionResetTab")
 
 function unlockContent() {
-    constructionContent.style.display = (hasContent("Construction")) ? "block" : "none"
+    constructionContent.style.display = (Data.shards.gte(1000) || hasContent("Construction")) ? "inline-block" : "none"
 }
 
 setInterval(unlockContent, 100)
